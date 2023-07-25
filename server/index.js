ed55e92e4448b9170fee2708f8de2ae5e9492408
Thabiso-007 <thabiso.hlatshwayo24@gmail.com>
2023-07-25 12:37:46 +0200

    modified (#3)

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
 const express = require('express')
 
-require('child_process')
+require('colors')
 require('dotenv').config()
+
+const dbConnection = require('./config/connection/db')
 
 const { server_port } = require('./config/env')
 
 const port = server_port || 5000
 
 const app = express()
-// 14:13
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}.`)
-})
 
+dbConnection()
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}.`.white.bold)
+})
