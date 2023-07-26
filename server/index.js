0f46c6d615c5fe0cd72a1f67a099edd59535585d
Thabiso-007 <thabiso.hlatshwayo24@gmail.com>
2023-07-26 18:52:09 +0200

    Modified (#3)

--- server/index.js
+++ server/index.js
@@ -1,17 +1,25 @@
 const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+const graphql = require('graphql')
 
 require('colors')
 require('dotenv').config()
 
-const dbConnection = require('./config/connection/db')
-
 const { server_port } = require('./config/env')
+const dbConnection = require('./config/connection/db')
+const RootQuery = require('./graphql/schemas')
+const RootMutation = require('./graphql/resolvers/index')
 
 const port = server_port || 5000
+const GraphSchema = graphql.GraphQLSchema
 
 const app = express()
-
 dbConnection()
+
+app.use('/graphql', graphqlHTTP({
+    schema: new GraphSchema({ query: RootQuery, mutation: RootMutation }),
+    graphiql: true
+}))
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}.`.white.bold)
