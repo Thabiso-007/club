b981a9a9e30869ea77d801142d57cc1da124bbed
Thabiso-007 <thabiso.hlatshwayo24@gmail.com>
2023-07-26 18:58:18 +0200

    Created resolvers file (#8)

---
+++ server/graphql/resolvers/index.js
@@ -0,0 +1,24 @@
+const { GraphQLObjectType, GraphQLString } = require('graphql')
+
+const { ClubType } = require('../types')
+const Club = require('../../models/ClubModel')
+
+const addClub = {
+    name: "addClub",
+    type: ClubType,
+    args: { name: { type: GraphQLString }, league: { type:GraphQLString }},
+    resolve: async (parent, args) => {
+        const club = new Club({ name: args.name, league: args.league })
+        const newClub = await club.save()
+        return newClub
+    }
+}
+
+const RootMutation = new GraphQLObjectType({
+    name: "RootMutation",
+    fields: { 
+        addClub
+    }
+})
+
+module.exports = RootMutation 
