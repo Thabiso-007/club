465b225f7822eda7a2a2d65ac418669214dec09b
Thabiso-007 <thabiso.hlatshwayo24@gmail.com>
2023-07-27 21:19:29 +0200

    Initial client commit

---
+++ client/src/components/Club.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container background">
+    <div class="row">
+     
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+ 
+  
+}
+</script>
+
+<style scoped>
+.background {
+  background-image: url("../assets/soccer.jpg");
+  background-color: #cccccc;
+  height: 100vh;
+  width: 100%;
+}
+</style>
