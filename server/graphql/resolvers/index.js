478671c800cdd473982fc66f863f5e6b0c85dc34
Thabiso-007 <thabiso.hlatshwayo24@gmail.com>
2023-07-27 18:55:32 +0200

    Updated relsovers (#8)

--- server/graphql/resolvers/index.js
+++ server/graphql/resolvers/index.js
@@ -1,8 +1,9 @@
-const { GraphQLObjectType, GraphQLString } = require('graphql')
+const { GraphQLObjectType, GraphQLString, GraphQLID } = require('graphql')
 
 const { ClubType } = require('../types')
 const Club = require('../../models/ClubModel')
 
+// Add a club
 const addClub = {
     name: "addClub",
     type: ClubType,
@@ -14,10 +15,37 @@ const addClub = {
     }
 }
 
+// Delete a club
+const deleteClub = {
+    name: "deleteClub",
+    type: ClubType,
+    args: { id: { type: GraphQLID }},
+    resolve: async (parent, args) => {
+        const removeClub = await Club.findByIdAndDelete(args.id)
+        return removeClub
+    }
+}
+
+// Update a club
+const updateClub = {
+    name: "updateClub",
+    type: ClubType,
+    args: { id: { type: GraphQLID }, name: { type: GraphQLString }, league: { type:GraphQLString }},
+    resolve: async (parent, args) => {
+        const update = await Club.findByIdAndUpdate(args.id,{
+            name: args.name,
+            league: args.league
+        })
+        return update
+    }
+}
+// 1:11:01
 const RootMutation = new GraphQLObjectType({
     name: "RootMutation",
     fields: { 
-        addClub
+        addClub,
+        deleteClub,
+        updateClub
     }
 })
 
