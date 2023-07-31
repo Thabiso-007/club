585883d38888cea3b880f08d5dcbdf47198044df
Thabiso-007 <thabiso.hlatshwayo24@gmail.com>
2023-07-31 14:36:19 +0200

    Initial client commit

---
+++ client/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/clubs">Clubs</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<style>
+#app {
+  width: 100%;
+  height: 100vh;
+  background: rgba(0,0,0,0.4);
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  width: 100%;
+  height: 100vh;
+  background-image: url("./assets/soccer.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: fixed;
+}
+</style>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container d-flex align-items-center justify-content-center">
+    <div class="row">
+      <div class="col-12">
+        <h1 class="text-white mt-5 text-center">Uefa champions league clubs</h1>
+        <div class="">
+          <form>
+            <div class="mb-3">
+              <label for="name" class="form-label">Club name</label>
+              <input type="text" class="form-control" id="name">
+            </div>
+            <div class="mb-3">
+              <label for="league" class="form-label">League</label>
+              <input type="text" class="form-control" id="league">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+ 
+  
+}
+</script>
