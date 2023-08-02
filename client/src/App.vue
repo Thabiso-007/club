12eb98902dd48b6e6ec642fcd5f0e30ca2a95393
Thabiso-007 <thabiso.hlatshwayo24@gmail.com>
2023-08-02 19:29:25 +0200

    Modified design (#10)

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,9 +4,6 @@
 
 <style>
 #app {
-  width: 100%;
-  height: 100vh;
-  background: rgba(0,0,0,0.4);
   margin: 0;
   padding: 0;
 }
@@ -18,9 +15,8 @@ body {
   width: 100%;
   height: 100vh;
   background-image: url("./assets/soccer.jpg");
-  background-repeat: no-repeat;
+  background-repeat: repeat;
   background-size: cover;
-  position: fixed;
 }
 
 @media only screen and (max-width: 865px) {
