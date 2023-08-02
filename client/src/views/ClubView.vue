6ba2f67544fb3c13f4f3aeb435778b9e1b262d15
Thabiso-007 <thabiso.hlatshwayo24@gmail.com>
2023-08-02 19:38:31 +0200

    Added club functionality (#20)

--- client/src/views/ClubView.vue
+++ client/src/views/ClubView.vue
@@ -1,29 +1,95 @@
 <template>
   <div class="container d-flex align-items-center justify-content-center">
-    <div class="w100 bg-white p-3 mt-5">
-      <div class="d-flex justify-content-center">
+    <div class="w100 bg-white p-3 mt-5 mb-5">
+      <div class="d-flex justify-content-center mb-3">
         <router-link to="/" class="btn btn-primary w-50">Add club</router-link>
       </div>
-      <ul>
-        <li class="w100 gap-2">
-          <div class="d-flex gap-5">
-            <p>Club name</p>
-            <p>League</p>
+      <ul class="w100 align-items-center justify-content-center">
+        <li 
+          class="gap-2 list"
+          v-for="(item, i) in state.clubList"
+          v-bind:key="i"
+          >
+          <div class="wrapper justify-content-between">
+            <div class="">
+              <h2 class="league">{{ item.league }}</h2>
+              <h3 class="club">{{ item.name }}</h3>
+            </div>
+            <div class="btn">
+              <button type="submit" class="btn btn-danger">Delete</button>
+              <button type="submit" class="btn btn-primary mt-2">Update</button>
+            </div>
           </div>
-          <button type="submit" class="btn btn-danger w25">Delete</button>
-          <button type="submit" class="btn btn-primary w25">Update</button>
+          <hr />
         </li>
       </ul>
     </div>
   </div>
 </template>
 
-<style scoped>
+<script>
+import { reactive, onMounted } from 'vue'
+import { getClubs } from '../graphql/queries'
+
+export default {
+  setup(props) {
+    const state = reactive({
+      clubs: {
+        name: null,
+        league: null
+      },
+      clubList: [],
+      update: false,
+      isLoading: false
+    });
+
+    onMounted(async() => {
+      const getAllClubs = await getClubs(state)
+      state.clubList = getAllClubs
+    })
+
+    return {
+      state
+    }
+  },
+};
+</script>
+
+<style>
 .w100 {
   width: 100%;
 }
 
-.w25 {
+.w15 {
   width: 15%;
+}
+
+.list {
+  list-style: none;
+}
+
+.wrapper {
+  width: 100%;
+  background-color: rgb(149, 168, 255);
+  padding: 10px;
+  display: flex;
+  flex-direction: row;
+  border-radius: 20px 0 20px 0;
+  justify-content: space-around;
+}
+
+.btn {
+  display: flex;
+  flex-direction: column;
+}
+
+.league {
+  font-size: 20px;
+  font-weight: 700;
+}
+
+.club {
+  font-size: 16px;
+  font-weight: 700;
 }
 </style>
