9c55b3b2d466be5757dc4085d340eb3ff5326a64
Thabiso-007 <thabiso.hlatshwayo24@gmail.com>
2023-08-09 20:32:02 +0200

    Modified (#17)

--- client/src/views/ClubView.vue
+++ client/src/views/ClubView.vue
@@ -1,42 +1,55 @@
 <template>
   <div>
     <div class="">
-      <img class="uefa" src="@/assets/uefa.png" alt="">
+      <router-link to="/">
+        <img class="uefa" src="@/assets/uefa.png" alt="">
+      </router-link>
     </div>
     <div class="container d-flex align-items-center justify-content-center">
       <div class="w100 bg-white p-3 mt-5 mb-5">
         <div class="d-flex justify-content-center mb-3">
           <router-link to="/" class="btn btn-primary w-50">Add club</router-link>
         </div>
-        <ul class="w100 align-items-center justify-content-center">
-          <li 
-            class="gap-2 list"
-            v-for="(item, i) in state.clubList"
-            v-bind:key="i"
-          >
-            <div class="wrapper justify-content-between">
-              <div>
-                <div class="d-flex gap-2">
-                  <img alt="Vue logo" class="logo" src="@/icons/europe.svg" width="20" height="20"/>
-                  <h2 class="league">{{ item.league }}</h2>
+        <div class="">
+          <ul class="w50 align-items-center justify-content-center">
+            <li 
+              class="gap-2 list"
+              v-for="(item, i) in state.clubList"
+              v-bind:key="i"
+            >
+              <div class="wrapper justify-content-between">
+                <div>
+                  <div class="d-flex gap-2">
+                    <img alt="Vue logo" class="logo" src="@/icons/europe.svg" width="20" height="20"/>
+                    <h2 class="league">{{ item.league }}</h2>
+                  </div>
+                  <div class="d-flex gap-2">
+                    <img alt="Vue logo" class="logo" src="@/icons/club.svg" width="20" height="20"/>
+                    <router-link :to="{name: 'club-detail', params: { id: item.id }}">
+                      <h3 class="club">{{ item.name }}</h3>
+                    </router-link>
+                  </div>
                 </div>
-                <div class="d-flex gap-2">
-                  <img alt="Vue logo" class="logo" src="@/icons/club.svg" width="20" height="20"/>
-                  <h3 class="club">{{ item.name }}</h3>
+                <div class="btn">
+                  <button 
+                    type="submit" 
+                    class="btn btn-danger"
+                    v-on:click="() => deleteClubHandler(e, item.id)"
+                  >Delete</button>
+                  <button 
+                    type="submit" 
+                    class="btn btn-primary mt-2"
+                    v-on:click="() => updateClubHandler(e, item.id)"
+                  >Update</button>
                 </div>
               </div>
-              <div class="btn">
-                <button 
-                  type="submit" 
-                  class="btn btn-danger"
-                  v-on:click="() => deleteClubHandler(e, item.id)"
-                >Delete</button>
-                <button type="submit" class="btn btn-primary mt-2">Update</button>
-              </div>
-            </div>
-            <hr />
-          </li>
-        </ul>
+              <hr />
+            </li>
+          </ul>
+        </div>
+        <!-- <div class="">
+            <h2>No Clubs</h2>
+        </div> -->
       </div>
     </div>
   </div>
@@ -44,13 +57,17 @@
 
 <script>
 import { reactive, onMounted } from 'vue'
+import { useRoute } from 'vue-router';
+
 import { getClubs } from '../graphql/queries'
 import { deleteClub } from '../graphql/mutations'
 
 export default {
   setup(props) {
+    const route = useRoute()
     const state = reactive({
       clubs: {
+        id: route.params.id,
         name: null,
         league: null
       },
@@ -65,15 +82,23 @@ export default {
     })
 
     const deleteClubHandler = async (e, itemId) => {
-      console.log(itemId)
+      // console.log(itemId)
       const deletedClub = await deleteClub(state, itemId)
       const getAllClubs = await getClubs(state)
       state.clubList = getAllClubs
     }
 
+    const updateClubHandler = async (e, id) => {
+       const selectedClub = state.clubList.filter((club, i) => club.id === id)[0]
+       state.clubs.name = selectedClub.name
+       state.clubs.league = selectedClub.league
+       state.update = true
+    }
+
     return {
       state,
-      deleteClubHandler
+      deleteClubHandler,
+      updateClubHandler,
     }
   },
 };
