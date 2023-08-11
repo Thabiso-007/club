5a9830aef91dff748768dd9cf1097096ebd58f3c
Thabiso-007 <thabiso.hlatshwayo24@gmail.com>
2023-08-11 13:00:05 +0200

    Added fetch all countries functionality (#9)

--- server/graphql/schemas/index.js
+++ server/graphql/schemas/index.js
@@ -1,6 +1,8 @@
 const { GraphQLObjectType, GraphQLList, GraphQLID } = require('graphql')
-const { ClubType } = require('../types') 
+
+const { ClubType, LeagueType } = require('../types') 
 const Club = require('../../models/ClubModel')
+const League = require('../../models/LeagueModel')
 
 // Get all clubs
 const clubs = {
@@ -23,11 +25,24 @@ const singleclub = {
     }
 }
 
+/** Admin section */
+
+// Get all clubs
+const countries = {
+    name: "Countries",
+    type: new GraphQLList(LeagueType),
+    resolve: async (parent, args) => {
+        const countries = await League.find()
+        return countries
+    }
+}
+
 const RootQuery = new GraphQLObjectType({
     name: "RootQuery",
     fields: {
         clubs,
-        singleclub
+        singleclub,
+        countries
     }
 })
 
