1ceda6599a0aa43e1db4853caf2c874e861da242
Thabiso-007 <thabiso.hlatshwayo24@gmail.com>
2023-08-11 12:58:34 +0200

    Added AddCountry, updatedCountry and deleteCountry functionailty (#8)

--- server/graphql/resolvers/index.js
+++ server/graphql/resolvers/index.js
@@ -1,7 +1,8 @@
 const { GraphQLObjectType, GraphQLString, GraphQLID } = require('graphql')
 
-const { ClubType } = require('../types')
+const { ClubType, LeagueType } = require('../types')
 const Club = require('../../models/ClubModel')
+const League = require('../../models/LeagueModel')
 
 // Add a club
 const addClub = {
@@ -40,12 +41,54 @@ const updateClub = {
     }
 }
 
+/** Admin section */
+
+// Add a country
+const addCountry = {
+    name: "addCountry",
+    type: LeagueType,
+    args: { country: { type: GraphQLString }, league: { type:GraphQLString }},
+    resolve: async (parent, args) => {
+        const country = new League({ country: args.country, league: args.league })
+        const newCountry = await country.save()
+        return newCountry
+    }
+}
+
+// Update a country
+const updateCountry = {
+    name: "updateCountry",
+    type: LeagueType,
+    args: { id: { type: GraphQLID }, country: { type: GraphQLString }, league: { type:GraphQLString }},
+    resolve: async (parent, args) => {
+        const update = await League.findByIdAndUpdate(args.id,{
+            country: args.country,
+            league: args.league
+        })
+        return update
+    }
+}
+
+// Delete a club
+const deleteCountry = {
+    name: "deleteCountry",
+    type: LeagueType,
+    args: { id: { type: GraphQLID }},
+    resolve: async (parent, args) => {
+        const removeCountry = await League.findByIdAndDelete(args.id)
+        return removeCountry
+    }
+}
+
 const RootMutation = new GraphQLObjectType({
     name: "RootMutation",
     fields: { 
         addClub,
         deleteClub,
-        updateClub
+        updateClub,
+        addCountry,
+        updateCountry,
+        deleteCountry
     }
 })
 
