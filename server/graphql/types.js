80ae71bf50f03b721e3f4f3d892b7cc1c54b486f
Thabiso-007 <thabiso.hlatshwayo24@gmail.com>
2023-08-13 16:37:03 +0200

    Added player type (#26)

--- server/graphql/types.js
+++ server/graphql/types.js
@@ -1,24 +1,53 @@
 const { GraphQLString, GraphQLID, GraphQLObjectType } = require('graphql')
 
+const User = require('../models/UserModel')
+
+const UserType = new GraphQLObjectType({
+    name: "UserType",
+    fields: () => ({
+        id: { type: GraphQLID },
+        username: { type: GraphQLString },
+        email: { type: GraphQLString },
+        displayName: { type: GraphQLString }
+    })
+})
+
 const ClubType = new GraphQLObjectType({
     name: "ClubType",
     fields: () => ({
-        id: {type: GraphQLID },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
         league: { type: GraphQLString }
+    })
+})
+
+const PlayerType = new GraphQLObjectType({
+    name: "PlayerType",
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        players: { type: GraphQLString }
     })
 })
 
 const LeagueType = new GraphQLObjectType({
     name: "LeagueType",
     fields: () => ({
-        id: {type: GraphQLID },
+        id: { type: GraphQLID },
         country: { type: GraphQLString },
-        league: { type: GraphQLString }
-    })
+        league: { type: GraphQLString },
+        admin: {
+            type: UserType,
+            resolve: (parent, args) => {
+                return User.findById(parent.adminId)
+            }
+        }
+    }) 
 })
 
 module.exports = { 
     ClubType,
-    LeagueType 
+    LeagueType,
+    UserType,
+    PlayerType 
 }
